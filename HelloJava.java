7d40774fed8da711c65ecb370d42a393beda9f96
hitesh suyal <hitesh.suyal@kritikalvision.ai>
2020-03-05 16:25:11 +0530

    dev2commit

---
+++ HelloJava.java
@@ -0,0 +1,13 @@
+public class HelloJava {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello Java");
+		System.out.println("My Dev Branch");
+		System.out.println("Conflict change");
+		  
+	// part 2 changes
+
+	}
+
+}
