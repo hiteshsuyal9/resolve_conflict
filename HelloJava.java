f70a7697819de7fb7fb1cffe44ede7244729fd8e
hitesh suyal <hitesh.suyal@kritikalvision.ai>
2020-03-05 16:39:28 +0530

    dev1 conflict changes

--- HelloJava.java
+++ HelloJava.java
@@ -5,6 +5,7 @@ public class HelloJava {
 		System.out.println("Hello Java");
 		System.out.println("My Dev Branch");
 		System.out.println("Conflict change");
+		System.out.println("Conflict change dev1");
 		  
 	// part 2 changes
 
